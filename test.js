eb323bb18143d812909275928163fa18a35b354d
Ilya Buzunov <46969838+ZTex275@users.noreply.github.com>
2019-02-04 10:46:11 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,23 @@
+var a = [];
+var size = prompt("Сколько элементов должно быть в массиве?")
+for (var i=0; i<size; i++)
+    a[i] = parseInt(prompt("Введите целочисленные элементы массива:"));
+
+    function arraySum(array){
+        var sum = 0;
+            for(var i = 0; i < array.length; i++){
+                sum += array[i];
+            }
+        alert ("Сумма чисел равна: "+sum);
+        }
+        arraySum(a);
+
+        Array.min = function(a){
+            return Math.min.apply(Math, a);
+        };
+        alert("Минимальное значение равно: "+Math.min.apply(Math, a));
+
+        Array.max = function(a){
+            return Math.max.apply(Math, a);
+        };
+        alert("Максимальное значение равно: "+Math.max.apply(Math, a));
